b93918c14796932ffed2721fd2428cc540803b13
arenga <arengapinnatamarzuki@gmail.com>
2024-07-12 20:03:15 +0700

    fix KNN Model

--- models/KNearesNeighbors.py
+++ models/KNearesNeighbors.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def knn_param_selector():
 
-    n_neighbors = st.number_input("K_neighbors", 5, 20, 5, 1)
+    n_neighbors = st.number_input("K_neighbors", 1, 20, 1, 1)
     metric = st.selectbox(
         "metric", ("minkowski", "euclidean", "manhattan", "chebyshev", "mahalanobis")
     )
