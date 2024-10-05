03a32f740ce09fe119dfe6c030a4d377917dd19d
arenga <arengapinnatamarzuki@gmail.com>
2024-10-05 13:50:24 +0700

    delete login auth

--- App.py
+++ App.py
@@ -35,23 +35,8 @@ st.markdown("""
     unsafe_allow_html=True
 )
 
-        
-__login__obj = __login__(
-    auth_token="courier_auth_token",
-    company_name="Shims",
-    width=200,
-    height=250,
-    logout_button_name="Logout",
-    hide_menu_bool=False,
-    hide_footer_bool=False,
-    lottie_url="https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json",
-)
 
-LOGGED_IN = __login__obj.build_login_ui()
-
-if LOGGED_IN == True:
-
-    st.markdown("""
+st.markdown("""
     <style>
         .st-emotion-cache-1y4p8pa {
             max-width: 50rem !important;
@@ -77,30 +62,28 @@ if LOGGED_IN == True:
     unsafe_allow_html=True
 )
 
-    for page_link, label, icon in zip(sidebar['page_link'], sidebar['label'], sidebar['icon']):
-        st.sidebar.page_link(page_link, label=label, icon=icon)
+for page_link, label, icon in zip(sidebar['page_link'], sidebar['label'], sidebar['icon']):
+    st.sidebar.page_link(page_link, label=label, icon=icon)
 
-    st.title("WELCOME TO ANALYTIX! 👋")
-    st.write(
-        """
-        **AnalytiX** is an application designed for data scientists aiming to perform thorough testing and analysis in machine learning. It offers a user-friendly interface with a wide array of tools and features.
-        """
-    )
-    st.write("Let's get started!")
-    
-    file = st.file_uploader('Upload CSV Dataset Here')
-    if file:
-        df = pd.read_csv(file, index_col=None)
-        df.to_csv('dataset.csv', index=None)
-    
-    if os.path.exists('dataset.csv'):
-        df = pd.read_csv('dataset.csv', index_col=None)
-        st.dataframe(df)
+st.title("WELCOME TO ANALYTIX! 👋")
+st.write(
+    """
+    **AnalytiX** is an application designed for data scientists aiming to perform thorough testing and analysis in machine learning. It offers a user-friendly interface with a wide array of tools and features.
+    """
+)
+st.write("Let's get started!")
 
-        if st.button("Delete Dataset"):
-            os.remove('dataset.csv')
-            st.info("Dataset has been deleted.")
-            st.rerun()
-    else:
-        df = pd.DataFrame()
-   
+file = st.file_uploader('Upload CSV Dataset Here')
+if file:
+    df = pd.read_csv(file, index_col=None)
+    df.to_csv('dataset.csv', index=None)
+
+if os.path.exists('dataset.csv'):
+    df = pd.read_csv('dataset.csv', index_col=None)
+    st.dataframe(df)
+    if st.button("Delete Dataset"):
+        os.remove('dataset.csv')
+        st.info("Dataset has been deleted.")
+        st.rerun()
+else:
+    df = pd.DataFrame()
